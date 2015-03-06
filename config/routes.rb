fb255feaa07f0c9b282989a5fce44387329e4125
Alec Wilson <alec.f.wilson@gmail.com>
2015-03-06 10:58:03 -0800

    starts activation

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
-  # get    'spotify' => 'static_pages#spotify'
+  get    'spotify' => 'static_pages#spotify'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
