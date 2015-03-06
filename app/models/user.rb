bb526848ecfcaad674f349599a374aeacd42e840
Alec Wilson <alec.f.wilson@gmail.com>
2015-03-06 12:48:43 -0800

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -29,9 +29,10 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
@@ -46,9 +47,9 @@ class User < ActiveRecord::Base
       self.email = email.downcase
     end
 
-    # Creates and assigned the activation token and digest
+    # Creates and assigns the activation token and digest.
     def create_activation_digest
-      self.activation_token = User.new_token
+      self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
 end
